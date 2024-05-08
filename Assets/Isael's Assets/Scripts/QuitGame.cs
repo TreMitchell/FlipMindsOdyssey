4e110096d6d59bedfb26904d13cb3a1a9dee900c
iisael9 <iisael090@gmail.com>
2024-05-08 00:00:23 -0700

    Build Test

--- Assets/Isael's Assets/Scripts/QuitGame.cs
+++ Assets/Isael's Assets/Scripts/QuitGame.cs
@@ -1,10 +1,37 @@
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class Quit_Game : MonoBehaviour
+//{
+//    public void QuitGame(){
+//        Application.Quit();
+//    }
+//}
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Quit_Game : MonoBehaviour
+public class QuitGame : MonoBehaviour
 {
-    public void QuitGame(){
+    private static QuitGame instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void Quit()
+    {
         Application.Quit();
     }
 }
