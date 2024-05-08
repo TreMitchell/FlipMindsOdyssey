3647fe9a7de15aad3e64e64005ec499baadba425
iisael9 <iisael090@gmail.com>
2024-05-08 00:38:20 -0700

    QuitButton Prefab

--- Assets/Isael's Assets/Scripts/QuitGame.cs
+++ Assets/Isael's Assets/Scripts/QuitGame.cs
@@ -1,37 +1,38 @@
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
-
-//public class Quit_Game : MonoBehaviour
-//{
-//    public void QuitGame(){
-//        Application.Quit();
-//    }
-//}
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class QuitGame : MonoBehaviour
+public class Quit_Game : MonoBehaviour
 {
-    private static QuitGame instance;
-
-    private void Awake()
-    {
-        if (instance == null)
-        {
-            instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
-    }
-
-    public void Quit()
+    public void QuitGame()
     {
         Application.Quit();
     }
 }
+
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class QuitGame : MonoBehaviour
+//{
+//    private static QuitGame instance;
+
+//    private void Awake()
+//    {
+//        if (instance == null)
+//        {
+//            instance = this;
+//            DontDestroyOnLoad(gameObject);
+//        }
+//        else
+//        {
+//            Destroy(gameObject);
+//        }
+//    }
+
+//    public void Quit()
+//    {
+//        Application.Quit();
+//    }
+//}
